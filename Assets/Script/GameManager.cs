5207e49a7ed6f969b0539b6c1072cf1de99e3172
MSI\81804 <a1929776588@gmail.com>
2022-07-19 20:20:30 +0900

    マップ作りを少し進めた、ターンの切り替えができるようにしたい。

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum Turn
+    { 
+        GamePlay,
+        Result,
+        SelectItem,
+        SetItem,
+    }
+}
---
+++ Assets/Script/StartingPoint.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingPoint : MonoBehaviour
+{
+    [SerializeField] GameObject Player1Point;
+    [SerializeField] GameObject Player2Point;
+    [SerializeField] GameObject Player3Point;
+    [SerializeField] GameObject Player4Point;
+
+    [SerializeField] GameObject Player1;
+    [SerializeField] GameObject Player2;
+    [SerializeField] GameObject Player3;
+    [SerializeField] GameObject Player4;
+}
