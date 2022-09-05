17ea29bf9a9d1fc7c44ce409a58f9ffdc9f4f814
Itou <a1929776588@gmail.com>
2022-09-05 17:30:45 +0900

    [Update]DOTweenでリザルトの得点スライダーのバリューが変わるようになった

--- Assets/Script/Result/PointManager.cs
+++ Assets/Script/Result/PointManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class PointManager : MonoBehaviour
 {
@@ -12,9 +13,9 @@ public class PointManager : MonoBehaviour
     [SerializeField] GameObject P4;
 
     [SerializeField] float P1Point = 0;
-    float P2Point = 0;
-    float P3Point = 0;
-    float P4Point = 0;
+    [SerializeField] float P2Point = 0;
+    [SerializeField] float P3Point = 0;
+    [SerializeField] float P4Point = 0;
 
     [SerializeField] Slider P1Slider;
     [SerializeField] Slider P2Slider;
@@ -22,26 +23,24 @@ public class PointManager : MonoBehaviour
     [SerializeField] Slider P4Slider;
 
     public bool _isCheck;
+
+    [SerializeField] float _changeValueInterval;
     void Update()
     {
         if (gameManager.NowTurn == GameManager.Turn.Result)
         {
-            if (P1Slider.value < P1.GetComponent<Player1Move>().P1Score)
-            {
-                P1Slider.value += 0.1f;
-            }
-            if (P2Slider.value < P2.GetComponent<Player2Move>().P2Score)
-            {
-                P1Slider.value += 0.1f;
-            }
-            //if (P3Slider.value < P3.GetComponent<Player3Move>().P3Score)
-            //{
-            //    P1Slider.value += 0.1f;
-            //}
-            //if (P4Slider.value < P4.GetComponent<Player4Move>().P4Score)
-            //{
-            //    P1Slider.value += 0.1f;
-            //}
+            ChangeValue(P1Slider,P1.GetComponent<Player1Move>().P1Score);
+            ChangeValue(P2Slider,P2.GetComponent<Player2Move>().P2Score);
+            //ChangeValue(P3Slider,P3.GetComponent<Player3Move>().P3Score);
+            //ChangeValue(P4Slider,P4.GetComponent<Player4Move>().P4Score);
         }
+    }
+    void ChangeValue(Slider _slider, float value)
+    {
+        // DOTween.To() を使って連続的に変化させる
+        DOTween.To(() => _slider.value, // 連続的に変化させる対象の値
+            x => _slider.value = x, // 変化させた値 x をどう処理するかを書く
+            value, // x をどの値まで変化させるか指示する
+            _changeValueInterval);   // 何秒かけて変化させるか指示する
     }
 }
