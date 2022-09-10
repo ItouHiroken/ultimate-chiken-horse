69699a1c46c482a708f5b4c9d7b0a8fe7b69288c
Itou <a1929776588@gmail.com>
2022-09-10 19:13:13 +0900

    [Add]出来上がったアイテムをぷれふぁぶにした

--- Assets/Script/Item/DestroyItem.cs
+++ Assets/Script/Item/DestroyItem.cs
@@ -6,16 +6,13 @@ public class DestroyItem : MonoBehaviour
 {
     public GameManager.Turn Turn;
     [SerializeField, Tooltip("ゲームマネージャーから参照したい")] GameObject _gameManager;
-    public bool _isSelect=false;
+    public bool _isSelect = false;
     private void Update()
     {
         Turn = _gameManager.GetComponent<GameManager>().NowTurn;
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (Turn==GameManager.Turn.SetItem&&!_isSelect)
+        if (Turn == GameManager.Turn.SetItem && !_isSelect)
         {
-            Destroy(gameObject);
+            this.gameObject.transform.position=new Vector3(1000,1000,1000);
         }
     }
 }
