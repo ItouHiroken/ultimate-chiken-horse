a4059e5a8a132cba7e8b241f715bb56ac5e0fa79
Itou <a1929776588@gmail.com>
2022-09-24 14:25:35 +0900

    [Add]タイトル画面に戻る機能を追加した

---
+++ Assets/Script/Menu/BackToTitle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackToTitle : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _clip;
+    [SerializeField] string SceneName;
+    [SerializeField] float _time;
+    [SerializeField] float _a;
+    private void Update()
+    {
+        _a += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.R) || _a >= _time)
+        {
+            _audioSource.PlayOneShot(_clip);
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+
+}
